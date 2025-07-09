2ec167561553713749c2c3b8e079ab4d92188efe
phamviet1121 <phamviet4454@gmail.com>
2025-07-09 21:20:13 +0700

    mover_player1

---
+++ Assets/_Asset/script/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    public Animator anim;
+    void Start()
+    {
+       // rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float hoz = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(hoz, 0, ver).normalized;
+        if (moveDirection.magnitude > 0.01f)
+        {
+            // Tạo hướng quay theo hướng di chuyển
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+            // Chỉ giữ lại góc quay quanh trục Y (khóa X, Z)
+            Vector3 euler = targetRotation.eulerAngles;
+            euler.x = 0;
+            euler.z = 0;
+            targetRotation = Quaternion.Euler(euler);
+
+            // Quay vật thể mượt theo hướng đó
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        }
+
+        Vector3 targetPosition = rb.position + moveDirection * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(targetPosition);
+        anim.SetFloat("mover", moveDirection.magnitude);
+
+
+    }
+}
